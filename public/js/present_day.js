9c97d4de7e5dcec2cf43eff3388d96934cffde23
Arleta Madej <arleta.madej@gmail.com>
2022-09-30 12:21:52 +0200

    deleted one bag in js

--- public/js/present_day.js
+++ public/js/present_day.js
@@ -2,20 +2,27 @@
  * set present date
  */
 
-function set_today()
-	{
-	var dzisiaj = new Date();
-	var dzien = dzisiaj.getDate();
-	var miesiac = dzisiaj.getMonth()+1;
-	var rok = dzisiaj.getFullYear();
-	
-	if (miesiac<10) miesiac= "0"+miesiac;
-	
-	if (dzien<10) dzien ="0"+dzien;
+ var dzisiaj = new Date();
+ var dzien = dzisiaj.getDate();
+ var miesiac = dzisiaj.getMonth()+1;
+ var rok = dzisiaj.getFullYear();
+ 
+ if (miesiac<10) miesiac= "0"+miesiac;
+ 
+ if (dzien<10) dzien ="0"+dzien;
 
-	var today = rok +"-"+ miesiac+ "-"+ dzien ;
-	
-	document.getElementById("today").value = rok +"-"+ miesiac+ "-"+ dzien ; 
+ var today = rok +"-"+ miesiac+ "-"+ dzien ;
+
+
+function set_today()
+{
+
+	document.getElementById("today").value = rok +"-"+ miesiac+ "-"+ dzien; 
 	document.getElementById("today").setAttribute("max", today);
-	}
+
+}
+
+
+
+
 	
