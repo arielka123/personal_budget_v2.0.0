eebbb6a3a82883b266758d566a2204f851454696
Arleta Madej <arleta.madej@gmail.com>
2022-11-01 20:43:51 +0100

    added tables with data

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -4,20 +4,25 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Models\Expenses;   
 use \App\Models\Incomes;
+use \App\Auth;
 
 class Settings extends Authenticated 
 {
     protected function before()
     {
         parent::before();
+
     }
 
     public static function newAction()      
     {         
+            $user = Auth::getUser();
+
             $args = [
             'expenseCategories' => Expenses::loadExpenseCategories(),
             'payments' => Expenses::loadUserpayments(),
-            'IncomeCategories' => Incomes::loadIncomeCategories()
+            'incomeCategories' => Incomes::loadIncomeCategories(),
+            'user' => $user,
         ];              
         //print_r (Expenses::loadUserExpenses());
             View::renderTemplate('Settings/new.html', $args);   
