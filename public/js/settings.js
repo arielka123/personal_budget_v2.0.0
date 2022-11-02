697b2df2876284c53207767acd48082ce8bf196b
Arleta Madej <arleta.madej@gmail.com>
2022-11-02 19:52:15 +0100

    add change icon of arrow after click

--- public/js/settings.js
+++ public/js/settings.js
@@ -19,11 +19,15 @@ function  showExpenseCategory() {
     if (expenseCategory.style.display === "none") {
 
         expenseCategory.style.display = "block";
- 
+        expenseCategoryIcon.classList.remove("icon-down-dir");
+        expenseCategoryIcon.classList.add("icon-up-dir");
+
     } 
     else {
         
         expenseCategory.style.display = "none";
+        expenseCategoryIcon.classList.add("icon-down-dir");
+        expenseCategoryIcon.classList.remove("icon-up-dir");
     }
 }
 
@@ -32,11 +36,15 @@ function  showIncomeCategory() {
     if (incomeCategory.style.display === "none") {
 
         incomeCategory.style.display = "block";
+        incomeCategoryIcon.classList.remove("icon-down-dir");
+        incomeCategoryIcon.classList.add("icon-up-dir");
  
     } 
     else {
         
         incomeCategory.style.display = "none";
+        incomeCategoryIcon.classList.add("icon-down-dir");
+        incomeCategoryIcon.classList.remove("icon-up-dir");
     }
 }
 
@@ -45,10 +53,14 @@ function  showPayments() {
     if (payments.style.display === "none") {
 
         payments.style.display = "block";
+        paymentsIcon.classList.remove("icon-down-dir");
+        paymentsIcon.classList.add("icon-up-dir");
  
     } 
     else {
         payments.style.display = "none";
+        paymentsIcon.classList.add("icon-down-dir");
+        paymentsIcon.classList.remove("icon-up-dir");
     }
 }
 
@@ -57,10 +69,14 @@ function  showProfile() {
     if (profile.style.display === "none") {
 
         profile.style.display = "block";
+        profileIcon.classList.remove("icon-down-dir");
+        profileIcon.classList.add("icon-up-dir");
  
     } 
     else {
         profile.style.display = "none";
+        profileIcon.classList.add("icon-down-dir");
+        profileIcon.classList.remove("icon-up-dir");
     }
 }
 
